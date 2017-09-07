6af66efb76300263558d82d8a1d9fd1560f559f4
Adam Ziegele <adamziegele@Adams-MacBook-Pro.local>
2017-09-07 19:42:08 -0400

    hello

---
+++ javascript.js
@@ -0,0 +1,25 @@
+// function hello(name) {
+//   if name = "" {
+//     var string = "Hello World";
+//     return string;
+//   } else {
+//     var string = "hello" + name;
+//     return string;
+//   }
+// }
+//
+//
+//
+// {
+//
+// }
+// hello('Mustache');
+//
+//
+
+function madLib(name, subject){
+  var string = name + "'s favroite subject in school is " + subject + ".";
+  return string;
+};
+
+console.log(madLib('Patrick', 'art'));
